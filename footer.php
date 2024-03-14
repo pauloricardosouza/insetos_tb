53f2427334ae3f3d8b212c954be15bac0922fefa
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2024-03-14 17:42:59 -0300

    Add files via upload
    Atualizações efetuadas na aula do dia 14 de março de 2024.

---
+++ footer.php
@@ -0,0 +1,9 @@
+    </div>
+
+    <!-- Container que abriga o rodapé da página -->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema Web desenvolvido por Paulinho</p>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include("header.php") ?>
+
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+
+<?php include("footer.php") ?>
